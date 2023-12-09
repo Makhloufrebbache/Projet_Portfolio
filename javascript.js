7bf668be9d4e036613bddbeca82131e5b213a682
Makhloufrebbache <etudeca202@gmail.com>
2023-12-08 22:04:24 -0500

    Portfolio

---
+++ javascript.js
@@ -0,0 +1,110 @@
+
+function retourProjets(){
+    
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet1 = document.getElementById("detail_prj1");
+    projets.setAttribute("class"," ");
+    Detail_Projet1.setAttribute("class","visually-hidden");
+
+    let Detail_Projet2 = document.getElementById("detail_prj2");
+    projets.setAttribute("class"," ");
+    Detail_Projet2.setAttribute("class","visually-hidden");
+
+    let Detail_Projet3 = document.getElementById("detail_prj3");
+    projets.setAttribute("class"," ");
+    Detail_Projet3.setAttribute("class","visually-hidden");
+
+    let Detail_Projet4 = document.getElementById("detail_prj4");
+    projets.setAttribute("class"," ");
+    Detail_Projet4.setAttribute("class","visually-hidden");
+
+    let Detail_Projet5 = document.getElementById("detail_prj5");
+    projets.setAttribute("class"," ");
+    Detail_Projet5.setAttribute("class","visually-hidden");
+
+    let Detail_Projet6 = document.getElementById("detail_prj6");
+    projets.setAttribute("class"," ");
+    Detail_Projet6.setAttribute("class","visually-hidden");
+
+    let Detail_Projet7 = document.getElementById("detail_prj7");
+    projets.setAttribute("class"," ");
+    Detail_Projet7.setAttribute("class","visually-hidden");
+
+    let Detail_Projet8 = document.getElementById("detail_prj8");
+    projets.setAttribute("class"," ");
+    Detail_Projet8.setAttribute("class","visually-hidden");
+    }
+
+function afficherDétailProjet1(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet1 = document.getElementById("detail_prj1");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet1.setAttribute("class"," ");
+    
+}
+}
+
+function afficherDétailProjet2(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet2 = document.getElementById("detail_prj2");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet2.setAttribute("class"," ");
+}
+}
+
+function afficherDétailProjet3(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet3 = document.getElementById("detail_prj3");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet3.setAttribute("class"," ");
+}
+}
+
+
+function afficherDétailProjet4(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet4 = document.getElementById("detail_prj4");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet4.setAttribute("class"," ");
+}
+}
+
+function afficherDétailProjet5(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet5 = document.getElementById("detail_prj5");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet5.setAttribute("class"," ");
+}
+}
+
+function afficherDétailProjet6(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet6 = document.getElementById("detail_prj6");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet6.setAttribute("class"," ");
+}
+}
+
+function afficherDétailProjet7(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet7 = document.getElementById("detail_prj7");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet7.setAttribute("class"," ");
+}
+}
+
+function afficherDétailProjet8(){
+    let projets = document.getElementById("portfolio");
+    let Detail_Projet8 = document.getElementById("detail_prj8");
+if (projets.getAttribute("class"," ")) {
+    projets.setAttribute("class","visually-hidden");
+    Detail_Projet8.setAttribute("class"," ");
+}
+}
